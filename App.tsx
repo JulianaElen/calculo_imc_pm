65caa1c9f27b3a5b77dd9b3d124d351f50b530d2
Juliana Elen Silva Oliveira <d2021005540@unifei.edu.br>
2024-04-27 08:45:22 -0300

    Primeiro commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Faça a calcularaaaaaaaaaa</Text>
+    </View>
+  )
+}
