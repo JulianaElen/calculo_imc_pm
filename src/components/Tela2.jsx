af8e31a9292085e804a2da14c303db8e230691de
Juliana Elen Silva Oliveira <d2021005540@unifei.edu.br>
2024-05-05 09:14:01 -0300

    O calculo do imc esta sendo feito e mostrado na tela final, assim como a classificação.

--- src/components/Tela2.jsx
+++ src/components/Tela2.jsx
@@ -1,26 +1,46 @@
-import { View, Text, Button, TextInput } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TextInput, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
 export default function Tela2(props) {
+
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+  const [imc, setImc] = useState(0);
+
+  useEffect( () => {
+    if((parseFloat(peso) > 0) && parseFloat(altura) > 0){
+      let calc = peso / (altura*altura);
+      setImc(calc);
+    }
+  }, [peso, altura]);
+
+  function confirmarImc() {
+    if(imc > 0) {
+      props.navigation.navigate("TelaResultado", {valorImc: imc})
+    } else {
+      Alert.alert("Digite peso e altura antes de calcular.")
+    }
+  }
+
   return (
     <View>
       <Text>Altura</Text>
       <TextInput
-        //  onChangeText={}
+        onChangeText={(valor) => {setAltura(valor)}}
         placeholder="dica"
         keyboardType="numeric"
         inputMode='numeric'
       />
       <Text>Peso</Text>
       <TextInput
-        //  onChangeText={}
+        onChangeText={(valor) => {setPeso(valor)}}
         placeholder="dica"
         keyboardType="numeric"
         inputMode='numeric'
       />
       <Button
         title='Calcular'
-        onPress={() => props.navigation.navigate("TelaResultado")}
+        onPress={() => confirmarImc()}
         color={'green'} />
     </View>
   )
--- src/components/Tela3.jsx
+++ src/components/Tela3.jsx
@@ -1,14 +1,36 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, Button } from 'react-native';
 
 export default function Tela3(props) {
+  const { route } = props;
+  const { valorImc } = route.params;
+  const classificacao = getClassificacaoImc(valorImc);
+
+  function getClassificacaoImc(imc) {
+    if (imc < 18.5) {
+      return "Abaixo do peso";
+    } else if (imc >= 18.5 && imc < 25) {
+      return "Peso normal";
+    } else if (imc >= 25 && imc < 30) {
+      return "Sobrepeso";
+    } else if (imc >= 30 && imc < 35) {
+      return "Obesidade grau I";
+    } else if (imc >= 35 && imc < 40) {
+      return "Obesidade grau II";
+    } else {
+      return "Obesidade grau III";
+    }
+  }
+
   return (
     <View>
       <Text>Resultado</Text>
+      <Text>Seu IMC é: {valorImc}</Text>
+      <Text>Classificação: {classificacao}</Text>
       <Button
         title='Início'
         onPress={() => props.navigation.navigate("TelaInicial")}
         color={'green'} />
     </View>
-  )
+  );
 }
